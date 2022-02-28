e7a8b096de4bd8b67696b9d7885f6736f4eae424
Xavier123157 <79547788+Xavier123157@users.noreply.github.com>
2022-02-28 20:54:35 +1100

    Add files via upload

---
+++ flexbox/script.js
@@ -0,0 +1,17 @@
+function display_ct5() {
+    var x = new Date()
+    var ampm = x.getHours( ) >= 12 ? ' PM' : ' AM';
+    
+    var x1=x.getMonth() + 1+ "/" + x.getDate() + "/" + x.getFullYear(); 
+    x1 = x1 + " - " +  x.getHours( )+ ":" +  x.getMinutes() + ":" +  x.getSeconds() + ":" + ampm;
+    document.getElementById('ct5').innerHTML = x1;
+    display_c5();
+     }
+     function display_c5(){
+    var refresh=1000; 
+    mytime=setTimeout('display_ct5()',refresh)
+    }
+    display_c5()
+
+
+
